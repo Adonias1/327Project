ce9791b1e0c9ac85e8b8230e8918b26d2ab5a056
Adonias Lopez <csuadonias@yahoo.com>
2017-05-08 16:15:26 -0700

    Fixes from assignment 6

--- src/client/TCPClient.java
+++ src/client/TCPClient.java
@@ -6,10 +6,10 @@ import java.net.*;
 class TCPClient{
 	public static void main(String argv[]) throws Exception{
 		String serverResponse; // String to hold response from server
-		Socket clientSocket = new Socket("192.168.1.13", 4279); // Establishing connection to server
+		Socket clientSocket = new Socket("localhost", 4279); // Establishing connection to server
 		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); // Creating a DataOutputStream to send requests to the server
 		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // Creating a BufferedReader  to receive responses from the server
-		
+		while(true){
 		for(int i = 1; i <= 5; i++){ // For loop for sending command "1" and receiving Fibonacci number
 			outToServer.writeBytes("1\n"); // Send command "1" to the server
 			serverResponse = inFromServer.readLine(); // Receiving response from server
@@ -25,6 +25,8 @@ class TCPClient{
 			serverResponse = inFromServer.readLine(); // Receiving response from server
 			System.out.println("Prime Number from Server: " + serverResponse); // Printing response from server
 	    }
-		clientSocket.close(); //Close client connection with the server
+		}
+		//clientSocket.close(); //Close client connection with the server
 	}
+	
 }
